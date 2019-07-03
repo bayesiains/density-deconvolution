f3c359d8f70e024490eb941e242ad514701812ab
James Ritchie <git@james.jmsrt.ch>
2019-07-03 15:04:49 +0100

    Add function to plot covariances.

---
+++ gmm/plotting.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib as mpl
+
+
+def plot_covariance(mean, cov, ax, alpha=0.5, color=None):
+    v, w = np.linalg.eigh(cov)
+    u = w[0] / np.linalg.norm(w[0])
+    angle = np.arctan2(u[1], u[0]) * 180 / np.pi
+    v = 2. * np.sqrt(2.) * np.sqrt(v)
+    ell = mpl.patches.Ellipse(
+        mean,
+        v[0],
+        v[1],
+        180 + angle,
+        color=color
+    )
+    ell.set_clip_box(ax.bbox)
+    ell.set_alpha(alpha)
+    ax.add_artist(ell)
