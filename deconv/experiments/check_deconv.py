ec5082067cbb24edd635f36f22ded01d267ec825
James Ritchie <git@james.jmsrt.ch>
2019-07-04 14:46:33 +0100

    Wrap experiments into functions.

--- deconv/experiments/check_deconv.py
+++ deconv/experiments/check_deconv.py
@@ -7,60 +7,66 @@ import seaborn as sns
 from deconv.gmm.plotting import plot_covariance
 from deconv.gmm.deconv_gmm import DeconvGMM
 
-D = 2
-K = 3
 
-N = 200
+def check_deconv_gmm(D, K, N, plot=False):
 
-means = (np.random.rand(K, D) * 20) - 10
-q = (2 * np.random.randn(K, D, D))
-covars = np.matmul(q.swapaxes(1,2), q)
+    means = (np.random.rand(K, D) * 20) - 10
+    q = (2 * np.random.randn(K, D, D))
+    covars = np.matmul(q.swapaxes(1, 2), q)
 
-qn = (0.1 * np.random.randn(N, K, D, D))
-noise_covars = np.matmul(qn.swapaxes(2, 3), qn)
+    qn = (0.1 * np.random.randn(N, K, D, D))
+    noise_covars = np.matmul(qn.swapaxes(2, 3), qn)
 
+    X = np.empty((N, K, D))
 
-X = np.empty((N, K, D))
-
-fig, ax = plt.subplots()
-
-for i in range(K):
-    X[:, i, :] = np.random.multivariate_normal(
-        mean=means[i, :],
-        cov=covars[i, :, :],
-        size=N
-    )
-    for j in range(N):
-        X[j, i, :] += np.random.multivariate_normal(
-            mean=np.zeros(D),
-            cov=noise_covars[j, i, :, :]
+    for i in range(K):
+        X[:, i, :] = np.random.multivariate_normal(
+            mean=means[i, :],
+            cov=covars[i, :, :],
+            size=N
         )
-    sc = ax.scatter(X[:, i, 0], X[:, i, 1], alpha=0.2, marker='x')
-    plot_covariance(
-        means[i, :],
-        covars[i, :, :],
-        ax,
-        color=sc.get_facecolor()[0]
+        for j in range(N):
+            X[j, i, :] += np.random.multivariate_normal(
+                mean=np.zeros(D),
+                cov=noise_covars[j, i, :, :]
+            )
+
+    data = (
+        torch.from_numpy(X.reshape(-1, D).astype(np.float32)),
+        torch.from_numpy(noise_covars.reshape(-1, D, D).astype(np.float32))
     )
 
-data = (
-    torch.from_numpy(X.reshape(-1, D).astype(np.float32)),
-    torch.from_numpy(noise_covars.reshape(-1, D, D).astype(np.float32))
-)
+    gmm = DeconvGMM(K, D, max_iters=1000)
+    gmm.fit(data)
+
+    if plot:
+        fig, ax = plt.subplots()
+
+        for i in range(K):
+            sc = ax.scatter(X[:, i, 0], X[:, i, 1], alpha=0.2, marker='x')
+            plot_covariance(
+                means[i, :],
+                covars[i, :, :],
+                ax,
+                color=sc.get_facecolor()[0]
+            )
+
+        sc = ax.scatter(gmm.means[:, 0], gmm.means[:, 1], marker='+')
+
+        for i in range(K):
+            plot_covariance(
+                gmm.means[i, :],
+                gmm.covars[i, :, :],
+                ax,
+                color=sc.get_facecolor()[0]
+            )
+
+        plt.show()
 
 
-gmm = DeconvGMM(K, D, max_iters=1000)
-gmm.fit(data)
-
-sc = ax.scatter(gmm.means[:, 0], gmm.means[:, 1], marker='+')
-
-
-for i in range(K):
-    plot_covariance(
-        gmm.means[i, :],
-        gmm.covars[i, :, :],
-        ax,
-        color=sc.get_facecolor()[0]
-    )
-
-plt.show()
+if __name__ == '__main__':
+    sns.set()
+    D = 2
+    K = 3
+    N = 200
+    check_deconv_gmm(D, K, N, plot=True)
--- deconv/experiments/check_gmm.py
+++ deconv/experiments/check_gmm.py
@@ -3,52 +3,60 @@ import torch
 
 import matplotlib.pyplot as plt
 import seaborn as sns
-sns.set()
 
 from deconv.gmm.plotting import plot_covariance
 from deconv.gmm.gmm import GMM
 
-D = 2
-K = 3
 
-N = 200
+def check_gmm(D, K, N, plot=False):
 
-means = (np.random.rand(K, D) * 20) - 10
-q = (2 * np.random.randn(K, D, D))
-covars = np.matmul(q.swapaxes(1, 2), q)
+    means = (np.random.rand(K, D) * 20) - 10
+    q = (2 * np.random.randn(K, D, D))
+    covars = np.matmul(q.swapaxes(1, 2), q)
 
-X = np.empty((N, K, D))
+    X = np.empty((N, K, D))
 
-fig, ax = plt.subplots()
+    for i in range(K):
+        X[:, i, :] = np.random.multivariate_normal(
+            mean=means[i, :],
+            cov=covars[i, :, :],
+            size=N
+        )
 
-for i in range(K):
-    X[:, i, :] = np.random.multivariate_normal(
-        mean=means[i, :],
-        cov=covars[i, :, :],
-        size=N
-    )
-    sc = ax.scatter(X[:, i, 0], X[:, i, 1], alpha=0.2, marker='x')
-    plot_covariance(
-        means[i, :],
-        covars[i, :, :],
-        ax,
-        color=sc.get_facecolor()[0]
-    )
+    X_data = torch.from_numpy(X.reshape(-1, D).astype(np.float32))
 
-X_data = torch.from_numpy(X.reshape(-1, D).astype(np.float32))
+    gmm = GMM(K, D, max_iters=1000)
+    gmm.fit((X_data,))
 
-gmm = GMM(K, D, max_iters=1000)
-gmm.fit((X_data,))
+    if plot:
+        fig, ax = plt.subplots()
 
-sc = ax.scatter(gmm.means[:, 0], gmm.means[:, 1], marker='+')
+        for i in range(K):
+            sc = ax.scatter(X[:, i, 0], X[:, i, 1], alpha=0.2, marker='x')
+            plot_covariance(
+                means[i, :],
+                covars[i, :, :],
+                ax,
+                color=sc.get_facecolor()[0]
+            )
+
+        sc = ax.scatter(gmm.means[:, 0], gmm.means[:, 1], marker='+')
+
+        for i in range(K):
+
+            plot_covariance(
+                gmm.means[i, :],
+                gmm.covars[i, :, :],
+                ax,
+                color=sc.get_facecolor()[0]
+            )
+
+        plt.show()
 
 
-for i in range(K):
-    plot_covariance(
-        gmm.means[i, :],
-        gmm.covars[i, :, :],
-        ax,
-        color=sc.get_facecolor()[0]
-    )
-
-plt.show()
+if __name__ == '__main__':
+    sns.set()
+    D = 2
+    K = 3
+    N = 200
+    check_gmm(D, K, N, plot=True)
