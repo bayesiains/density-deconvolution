35be2a2e4e4ccb00f03f48a32b2fd53290f8217c
James Ritchie <git@james.jmsrt.ch>
2019-11-15 16:49:02 +0000

    Change train set size.

--- deconv/experiments/gaia/variable_n/fit_sgd.sh
+++ deconv/experiments/gaia/variable_n/fit_sgd.sh
@@ -12,4 +12,4 @@
 source ~/.bashrc
 conda activate deconv
 
-python deconv/experiments/gaia/variable_n/fit_gaia_sgd.py -c 512 -b 512 -e 10 -l 0.001 -w=0.001 -k 5 --lr-step 10 --lr-gamma 0.1 --train-limit 25000000 --use-cuda /disk/scratch/s0904254/gaia_source_shuffled.h5 results/variable_n/sgd_512_25_${SLURM_JOBID}
+python deconv/experiments/gaia/variable_n/fit_gaia_sgd.py -c 512 -b 512 -e 10 -l 0.001 -w=0.001 -k 5 --lr-step 10 --lr-gamma 0.1 --train-limit 100000000 --use-cuda /disk/scratch/s0904254/gaia_source_shuffled.h5 results/variable_n/sgd_512_100_${SLURM_JOBID}
