52d0eee70297729bc4f2b1d7349a45ecebf708e2
James Ritchie <git@james.jmsrt.ch>
2020-05-19 11:26:37 +0100

    Add option to pass data to sampling function.

--- deconv/utils/sampling.py
+++ deconv/utils/sampling.py
@@ -2,13 +2,22 @@ import torch
 
 
     
-def minibatch_sample(sample_f, num_samples, batch_size, device=torch.device('cpu'), 
-                     **kwargs):
+def minibatch_sample(sample_f, num_samples, dimensions, batch_size, device=torch.device('cpu'), context=None, x=None):
 
-    samples = torch.zeros((num_samples, self.dimensions), device=device)
+    if context is not None:
+        ld = context.shape[0]
+    else:
+        ld = 1
+    
+    samples = torch.zeros((ld, num_samples, dimensions), device=device)
 
     for i in range(-(-num_samples // batch_size)):
         start = i * batch_size
         stop = (i + 1) * batch_size
         n = min(batch_size, num_samples - start)
-        samples[start:stop] = sample_f(n, **kwargs).to(device)
+        if x is None:
+            samples[:, start:stop, :] = sample_f(n, context=context).to(device)
+        else:
+            samples[:, start:stop, :] = sample_f(x, n, context=context).to(device)
+        
+    return samples
